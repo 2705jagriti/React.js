c068528d7d6aabc073e31f944908b0b07632d604
2705jagriti <jagritichaudhary842@gmail.com>
2024-08-22 10:48:30 +0530

    ..

---
+++ 01_tut/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import Content from "./content";
+import Footer from "./Footer"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
