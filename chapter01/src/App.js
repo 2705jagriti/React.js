3fc634e142dd45e5ed1783aa23afb4d61760423b
2705jagriti <jagritichaudhary842@gmail.com>
2025-02-04 00:19:34 +0530

    ..

--- chapter01/src/App.js
+++ chapter01/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to see 
+          Edit <code>src/App.js</code> and save to change
+
         </p>
         <a
           className="App-link"
@@ -17,9 +18,11 @@ function App() {
         >
           Learn React
         </a>
+      
       </header>
+      html
     </div>
-  );
+  ); 
 }
 
 export default App;
